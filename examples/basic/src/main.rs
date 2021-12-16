7cbcc17c6e03c4a731512dc83c78393409b7535c
Proudmuslim <proudmuslim-dev@protonmail.com>
2021-12-16 10:29:02 -0800

    new: Some garbage idk anymore

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,49 +1,11 @@
 use anyhow::Result;
-use async_trait::async_trait;
-use octocat_rs::github::{AuthMethod, GitHubApplication};
+
+use octocat_rs::github::{GitHub, GitHubApplication};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let github_client = GitHub::new("USERNAME", "OAUTH_TOKEN");
-    github_client.run().await?;
+    let github_client = GitHub::new("", "");
+    github_client.start().await?;
 
     Ok(())
-}
-
-/// An API client for github.
-#[allow(dead_code)]
-pub struct GitHub {
-    api_key: String,
-    username: Option<String>,
-    auth_method: AuthMethod,
-    http_client: reqwest::Client,
-}
-
-#[async_trait]
-impl GitHubApplication for GitHub {
-    fn new(username: &str, token: &str) -> Self {
-        Self {
-            api_key: token.to_owned(),
-            username: Some(username.to_owned()),
-            auth_method: AuthMethod::OAuthToken,
-            ..Default::default()
-        }
-    }
-
-    fn new_with_sso(token: &str) -> Self {
-        Self {
-            api_key: token.to_owned(),
-            username: None,
-            auth_method: AuthMethod::Sso,
-            ..Default::default()
-        }
-    }
-
-    fn current_auth_method(&self) -> AuthMethod {
-        self.auth_method
-    }
-
-    async fn run(&self) -> Result<()> {
-        todo!()
-    }
 }
--- src/github.rs
+++ src/github.rs
@@ -1,10 +1,77 @@
+use reqwest::{
+    header::{HeaderMap, USER_AGENT},
+    RequestBuilder,
+};
+
 use anyhow::Result;
 use async_trait::async_trait;
 
-#[derive(Debug, Copy, Clone)]
-pub enum AuthMethod {
-    OAuthToken,
-    Sso,
+/// Used to represent the current [authorization method](https://docs.github.com/en/rest/overview/resources-in-the-rest-api#authentication).
+#[derive(Debug, Clone)]
+pub enum Authorization {
+    Personal {
+        username: String,
+        access_token: String,
+    },
+    OAuth(String),
+    Sso(String),
+}
+
+/// A simple implementation of [`GitHubApplication`], intended for use in small applications.
+pub struct GitHub {
+    client: reqwest::Client,
+    auth: Authorization,
+}
+
+#[async_trait]
+impl GitHubApplication for GitHub {
+    fn new(username: &str, access_token: &str) -> Self {
+        let mut headers = HeaderMap::new();
+        headers.insert(USER_AGENT, "Mr. Wu Han".parse().unwrap()); // I'm a comedian
+
+        let client = reqwest::Client::builder()
+            .default_headers(headers)
+            .build()
+            .unwrap();
+
+        Self {
+            client,
+            auth: Authorization::Personal {
+                username: username.to_owned(),
+                access_token: access_token.to_owned(),
+            },
+        }
+    }
+
+    fn new_with_sso(_token: &str) -> Self {
+        todo!()
+    }
+
+    fn current_auth_method(&self) -> Authorization {
+        todo!()
+    }
+
+    fn http_request(&self, builder: RequestBuilder) -> RequestBuilder {
+        match self.auth.clone() {
+            Authorization::Personal {
+                username,
+                access_token,
+            } => builder.basic_auth(username, Some(access_token)),
+            _ => todo!(),
+        }
+    }
+
+    async fn run(&self) -> Result<()> {
+        let res = self
+            .http_request(self.client.get("https://api.github.com/user"))
+            .send()
+            .await?
+            .text()
+            .await?;
+
+        dbg!(res);
+        todo!()
+    }
 }
 
 // TODO: Work out how requests will be handled; will probably be done with some sort of util module
@@ -21,7 +88,10 @@ pub trait GitHubApplication {
     fn new_with_sso(token: &str) -> Self;
 
     /// Helper function for getting the current authorization method.
-    fn current_auth_method(&self) -> AuthMethod;
+    fn current_auth_method(&self) -> Authorization;
+
+    /// Wrapper function for getting an HTTP client; GitHub API requires the user agent header to be set.
+    fn http_request(&self, builder: reqwest::RequestBuilder) -> reqwest::RequestBuilder;
 
     /// The code that is run when your application starts. Called by [`start`].
     ///
