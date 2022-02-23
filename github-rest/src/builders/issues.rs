1811a16aa729feed789e164684e7dcbac2a47e19
Proudmuslim <proudmuslim-dev@protonmail.com>
2022-02-23 10:24:35 -0800

    wip(builders): Begin writing and using macros for builders

--- github-rest/src/builders/issues.rs
+++ github-rest/src/builders/issues.rs
@@ -1,100 +1,47 @@
 use crate::{
-    builders::Builder,
+    builders::{builder, builder_nested_setters, builder_nested_string_setters, builder_string_setters, Builder},
     methods::{get_issues, GetIssueBody, IssueState},
     model::issues::Issues,
     GithubRestError, Requester,
 };
 use async_trait::async_trait;
 
-/// * tags issues
-/// * get `/repos/{owner}/{repo}/issues`
-/// * docs <https://docs.github.com/rest/reference/issues#list-repository-issues>
-///
-/// List repository issues
-/// List issues in a repository.
-///
-/// **Note**: GitHub's REST API v3 considers every pull request an issue, but
-/// not every issue is a pull request. For this reason, "Issues" endpoints may
-/// return both issues and pull requests in the response. You can identify pull
-/// requests by the `pull_request` key. Be aware that the `id` of a pull
-/// request returned from "Issues" endpoints will be an _issue id_. To find out the pull request id, use the "[List pull requests](https://docs.github.com/rest/reference/pulls#list-pull-requests)" endpoint.
-#[derive(Default, Clone)]
-pub struct GetIssuesBuilder {
-    owner: String,
-    repo: String,
-    body: GetIssueBody,
-}
-
-impl GetIssuesBuilder {
-    pub fn new() -> Self {
-        Self::default()
+builder!(
+    /// * tags issues
+    /// * get `/repos/{owner}/{repo}/issues`
+    /// * docs <https://docs.github.com/rest/reference/issues#list-repository-issues>
+    ///
+    /// List repository issues
+    /// List issues in a repository.
+    ///
+    /// **Note**: GitHub's REST API v3 considers every pull request an issue,
+    /// but not every issue is a pull request. For this reason, "Issues"
+    /// endpoints may return both issues and pull requests in the response.
+    /// You can identify pull requests by the `pull_request` key. Be aware
+    /// that the `id` of a pull request returned from "Issues" endpoints will be an _issue id_. To find out the pull request id, use the "[List pull requests](https://docs.github.com/rest/reference/pulls#list-pull-requests)" endpoint.
+    GetIssuesBuilder {
+        owner: String,
+        repo: String,
+        body: GetIssueBody
     }
+);
 
-    pub fn milestone<T: Into<String>>(mut self, milestone: T) -> GetIssuesBuilder {
-        self.body.milestone = Some(milestone.into());
-        self
+builder_string_setters!(GetIssuesBuilder { owner, repo });
+builder_nested_setters!(GetIssuesBuilder { body { state: IssueState } });
+builder_nested_string_setters!(GetIssuesBuilder {
+    body {
+        milestone,
+        assignee,
+        creator,
+        mentioned,
+        labels,
+        sort,
+        direction,
+        since,
+        per_page,
+        page
     }
-
-    pub fn state(mut self, state: IssueState) -> GetIssuesBuilder {
-        self.body.state = Some(state);
-        self
-    }
-
-    pub fn assignee<T: Into<String>>(mut self, assignee: T) -> GetIssuesBuilder {
-        self.body.assignee = Some(assignee.into());
-        self
-    }
-
-    pub fn creator<T: Into<String>>(mut self, creator: T) -> GetIssuesBuilder {
-        self.body.creator = Some(creator.into());
-        self
-    }
-
-    pub fn mentioned<T: Into<String>>(mut self, mentioned: T) -> GetIssuesBuilder {
-        self.body.mentioned = Some(mentioned.into());
-        self
-    }
-
-    pub fn labels<T: Into<String>>(mut self, labels: T) -> GetIssuesBuilder {
-        self.body.labels = Some(labels.into());
-        self
-    }
-
-    pub fn sort<T: Into<String>>(mut self, sort: T) -> GetIssuesBuilder {
-        self.body.sort = Some(sort.into());
-        self
-    }
-
-    pub fn direction<T: Into<String>>(mut self, direction: T) -> GetIssuesBuilder {
-        self.body.direction = Some(direction.into());
-        self
-    }
-
-    pub fn since<T: Into<String>>(mut self, since: T) -> GetIssuesBuilder {
-        self.body.since = Some(since.into());
-        self
-    }
-
-    pub fn per_page(mut self, count: i32) -> GetIssuesBuilder {
-        self.body.per_page = Some(count.to_string());
-        self
-    }
-
-    pub fn page(mut self, page: i32) -> GetIssuesBuilder {
-        self.body.page = Some(page.to_string());
-        self
-    }
-
-    pub fn owner<T: Into<String>>(mut self, user: T) -> GetIssuesBuilder {
-        self.owner = user.into();
-        self
-    }
-
-    pub fn repo<T: Into<String>>(mut self, repo: T) -> GetIssuesBuilder {
-        self.repo = repo.into();
-        self
-    }
-}
+});
 
 #[async_trait]
 impl Builder for GetIssuesBuilder {
@@ -122,8 +69,8 @@ mod tests {
         let builder = GetIssuesBuilder::new()
             .owner("microsoft")
             .repo("vscode")
-            .per_page(1)
-            .page(2)
+            .per_page(1.to_string())
+            .page(2.to_string())
             .state(IssueState::Open);
 
         let r = builder.execute(&requester).await.unwrap();
--- github-rest/src/builders/mod.rs
+++ github-rest/src/builders/mod.rs
@@ -17,3 +17,129 @@ pub trait Builder {
     where
         T: Requester;
 }
+
+/// Define a struct. This expands to:
+///
+///     #[derive(Default, Clone)]
+///     pub struct S {
+///         a: Type,
+///         b: Type,
+///         c: Type,
+///     }
+macro_rules! builder {
+    (
+        $(#[$attr:meta])*
+        $name:ident { $($field:ident: $field_type:ident),* }
+    ) => {
+
+        $(#[$attr])*
+        #[derive(Default, Clone)]
+        pub struct $name {
+            $(
+                $field: $field_type,
+            )*
+        }
+
+        impl $name {
+            pub fn new() -> Self { Self::default() }
+        }
+    }
+}
+
+/// Build an impl block with setters. This expands to:
+///
+///     impl S {
+///         pub fn a(mut self, v: A) -> Self {
+///             self.a = v;
+///             self
+///         }
+///     }
+macro_rules! builder_setters {
+    ($name:ident { $($field:ident: $field_type:ident),* }) => {
+        paste::paste! {
+            impl $name {
+                $(
+                    pub fn $field(mut self, $field: $field_type) -> Self {
+                        self.$field = $field;
+                        self
+                    }
+                )*
+            }
+        }
+    }
+}
+
+/// Build an impl block with setters. This expands to:
+///
+///     impl S {
+///         pub fn a<T: Into<String>>(mut self, v: T) -> Self {
+///             self.a = v.into();
+///             self
+///         }
+///     }
+macro_rules! builder_string_setters {
+    ($name:ident { $($field:ident),* }) => {
+        paste::paste! {
+            impl $name {
+                $(
+                    pub fn $field<T: Into<String>>(mut self, $field: T) -> Self {
+                        self.$field = $field.into();
+                        self
+                    }
+                )*
+            }
+        }
+    }
+}
+
+/// Build an impl block with setters. This expands to:
+///
+///     impl S {
+///         pub fn a<T: Into<String>>(mut self, v: T) -> Self {
+///             self.f.a = Some(v.into());
+///             self
+///         }
+///     }
+macro_rules! builder_nested_string_setters {
+    ($name:ident { $field:ident { $($subfield:ident),* } }) => {
+        paste::paste! {
+            impl $name {
+                $(
+                    pub fn $subfield<T: Into<String>>(mut self, $subfield: T) -> Self {
+                        self.$field.$subfield = Some($subfield.into());
+                        self
+                    }
+                )*
+            }
+        }
+    }
+}
+
+/// Build an impl block with setters. This expands to:
+///
+///     impl S {
+///         pub fn a(mut self, v: A) -> Self {
+///             self.f.a = Some(v);
+///             self
+///         }
+///     }
+macro_rules! builder_nested_setters {
+    ($name:ident { $field:ident { $($subfield:ident: $subfield_type:ident),* } }) => {
+        paste::paste! {
+            impl $name {
+                $(
+                    pub fn $subfield(mut self, $subfield: $subfield_type) -> Self {
+                        self.$field.$subfield = Some($subfield);
+                        self
+                    }
+                )*
+            }
+        }
+    }
+}
+
+pub(crate) use builder;
+pub(crate) use builder_nested_setters;
+pub(crate) use builder_nested_string_setters;
+pub(crate) use builder_setters;
+pub(crate) use builder_string_setters;
